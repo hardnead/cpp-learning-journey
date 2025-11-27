53a6da7cc1bda1d6ca1a0c349fe543dde739a268
Danendra Hardjo Judanto <code.hardnead@gmail.com>
2025-11-27 21:55:10 +0700

    Add new file

---
+++ LearnCPP/Typedef.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<vi> vvi; // vector of vector integer
+typedef pair<int, int> pii;
+typedef string text; // "text" = alias untuk "string"
+int main(){
+    int total = 0; // No Declaration
+    text totals = to_string(total); 
+    text tota;
+    text firstName = "Danendra";
+    text middleName = "Hardjo";
+    text lastName = "Judanto";
+    totals = firstName + " " + middleName + " " + lastName;
+    tota = firstName + " " + middleName + " " + lastName;
+    cout << tota << "\n";
+    cout << totals;
+    return 0;
+}
