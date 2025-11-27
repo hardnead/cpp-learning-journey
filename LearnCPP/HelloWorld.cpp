5a0b5033c1aeb8c8291732a6cca5a04a9b51d36e
Danendra Hardjo Judanto <code.hardnead@gmail.com>
2025-11-27 21:45:46 +0700

    Add HelloWorld File to LearnCPP Folder

---
+++ LearnCPP/HelloWorld.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(){
+    // This is a comment
+    /*
+        This is a multiple comment
+    */
+    string x;
+    x = "Budiono Siregar", x = "Kapal Lawd"; // Yg dibaca Kapal Lawd
+    cout << x << "\n";
+    cout << "My name is Budiono Siregar aka Kapal Lawd" << "\n";
+    cout << "My name is Danendra Hardjo Judanto\n";
+    cout << "My GPA is 4.0\n";
+    return 0;
+}
